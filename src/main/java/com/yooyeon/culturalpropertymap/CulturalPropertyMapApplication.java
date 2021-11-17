3210e9aabf05700673e97e8c61e8736d0d8e1455
uyeonH <yooyeon0508@gmail.com>
2021-11-17 22:10:08 +0900

    Initial commit

---
+++ src/main/java/com/yooyeon/culturalpropertymap/CulturalPropertyMapApplication.java
@@ -0,0 +1,13 @@
+package com.yooyeon.culturalpropertymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalPropertyMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturalPropertyMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yooyeon/culturalpropertymap/CulturalPropertyMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooyeon.culturalpropertymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalPropertyMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
